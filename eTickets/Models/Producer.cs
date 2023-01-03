4fbfad407797ba2b9f7634fd38c2e1483837c018
HassanYousef1985 <31389144+HassanYousef1985@users.noreply.github.com>
2023-01-03 15:08:39 +0100

    Getting producer details

--- eTickets/Models/Producer.cs
+++ eTickets/Models/Producer.cs
@@ -1,8 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using eTickets.Data.Base;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace eTickets.Models
 {
-    public class Producer
+    public class Producer : IEntityBase
     {
         [Key]
         public int Id { get; set; }
--- eTickets/Startup.cs
+++ eTickets/Startup.cs
@@ -32,6 +32,8 @@ namespace eTickets
 
             //Services configuration
             services.AddScoped<IActorsService, ActorsService>();
+            services.AddScoped<IProducersService, ProducersService>();
+
 
             services.AddControllersWithViews();
 
