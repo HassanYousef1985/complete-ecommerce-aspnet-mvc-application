6a1d01a525f36bc7c523c8aea366a19392a0dcb6
HassanYousef1985 <31389144+HassanYousef1985@users.noreply.github.com>
2023-01-04 11:25:32 +0100

    Getting cinema details

--- eTickets/Controllers/CinemasController.cs
+++ eTickets/Controllers/CinemasController.cs
@@ -35,9 +35,17 @@ namespace eTickets.Controllers
         [HttpPost]
         public async Task<IActionResult> Create([Bind("Logo,Name,Description")] Cinema cinema)
         {
-           // if (!ModelState.IsValid) return View(cinema);
+            if (!ModelState.IsValid) return View(cinema);
             await _service.AddAsync(cinema);
             return RedirectToAction(nameof(Index));
+        }
+
+        //Get: Cinemas/Details/1
+        public async Task<IActionResult> Details(int id)
+        {
+            var cinemaDetails = await _service.GetByIdAsync(id);
+            if (cinemaDetails == null) return View("NotFound");
+            return View(cinemaDetails);
         }
     }
 }
