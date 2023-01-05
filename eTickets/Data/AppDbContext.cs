f26ca1fccf3b1722ddd2b6b030fcd669216dc25b
HassanYousef1985 <31389144+HassanYousef1985@users.noreply.github.com>
2023-01-05 14:03:04 +0100

    update

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -9,7 +9,6 @@ namespace eTickets.Data
 {
     public class AppDbContext : DbContext
     {
-      
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
