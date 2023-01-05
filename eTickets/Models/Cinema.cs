4220d3fea80aa78c0f4a3866c54d1d929ef4a518
HassanYousef1985 <31389144+HassanYousef1985@users.noreply.github.com>
2023-01-05 15:29:42 +0100

    Getting movie details 2

--- eTickets/Models/Cinema.cs
+++ eTickets/Models/Cinema.cs
@@ -25,6 +25,6 @@ namespace eTickets.Models
         public string Description { get; set; }
 
         //Relationships
-        public List<NewMovieVM> Movies { get; set; }
+        public List<Movie> Movies { get; set; }
     }
 }
